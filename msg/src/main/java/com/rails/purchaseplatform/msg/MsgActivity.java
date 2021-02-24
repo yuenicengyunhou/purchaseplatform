d86385a9cd80cf7c1b33d428119eacb1b88ba8c6
王超 <sk_comic@163.com>
2021-02-24 17:27:03 +0800

    创建消息模块

---
+++ msg/src/main/java/com/rails/purchaseplatform/msg/MsgActivity.java
@@ -0,0 +1,14 @@
+package com.rails.purchaseplatform.msg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MsgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg);
+    }
+}
