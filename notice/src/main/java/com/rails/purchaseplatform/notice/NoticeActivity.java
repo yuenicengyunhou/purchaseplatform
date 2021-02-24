de97aeb6174292afbbe9b8de0770fc1a59f41dca
王超 <sk_comic@163.com>
2021-02-24 17:15:54 +0800

    创建公告模块

---
+++ notice/src/main/java/com/rails/purchaseplatform/notice/NoticeActivity.java
@@ -0,0 +1,14 @@
+package com.rails.purchaseplatform.notice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+    }
+}
