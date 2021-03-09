5d222de024e9aceb036a24456ff96f99c0757f4f
王超 <sk_comic@163.com>
2021-03-09 10:27:58 +0800

    找回主页

---
+++ app/src/main/java/com/rails/purchaseplatform/MainActivity.java
@@ -0,0 +1,38 @@
+package com.rails.purchaseplatform;
+
+import android.os.Bundle;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.rails.purchaseplatform.common.ConRoute;
+import com.rails.purchaseplatform.databinding.ActivityMainBinding;
+import com.rails.purchaseplatform.framwork.base.BaseErrorActivity;
+
+
+/**
+ * tabs 页面
+ */
+@Route(path = ConRoute.RAILS.MAIN)
+public class MainActivity extends BaseErrorActivity<ActivityMainBinding> {
+
+
+    @Override
+    protected void initialize(Bundle bundle) {
+
+    }
+
+    @Override
+    protected int getColor() {
+        return 0;
+    }
+
+    @Override
+    protected boolean isSetSystemBar() {
+        return false;
+    }
+
+    @Override
+    protected boolean isBindEventBus() {
+        return false;
+    }
+
+}
