0229649add674a57e2fc35d88cc8edb39d30dddb
ZhangXiaofu <zhangsep24@163.com>
2021-03-15 09:46:01 +0800

    nothing

--- common/src/main/java/com/rails/purchaseplatform/common/multiplecolumnslist/MultipleColumnsListAdapter.java
+++ common/src/main/java/com/rails/purchaseplatform/common/multiplecolumnslist/MultipleColumnsListAdapter.java
@@ -16,18 +16,18 @@ public class MultipleColumnsListAdapter extends AbsMultipleColumnsListAdapter<St
     public void convert(MultipleColumnsListViewHolder holder, int[] positions, List<String> models) {
         for (int i = 0; i < positions.length; i++) {
             switch (i) {
-                case 0:
-                    holder.getLinearLayout(R.id.ll_layout_1).setVisibility(View.VISIBLE);
-                    holder.setText(R.id.tv_zuo, models.get(i).getSchool_id());
-                    break;
-                case 1:
-                    holder.getLinearLayout(R.id.ll_layout_2).setVisibility(View.VISIBLE);
-                    holder.setText(R.id.tv_you, models.get(i).getSchool_id());
-                    break;
-                case 2:
-                    holder.getLinearLayout(R.id.ll_layout_3).setVisibility(View.VISIBLE);
-                    holder.setText(R.id.tv_san, models.get(i).getSchool_id());
-                    break;
+//                case 0:
+//                    holder.getLinearLayout(R.id.ll_layout_1).setVisibility(View.VISIBLE);
+//                    holder.setText(R.id.tv_zuo, models.get(i).getSchool_id());
+//                    break;
+//                case 1:
+//                    holder.getLinearLayout(R.id.ll_layout_2).setVisibility(View.VISIBLE);
+//                    holder.setText(R.id.tv_you, models.get(i).getSchool_id());
+//                    break;
+//                case 2:
+//                    holder.getLinearLayout(R.id.ll_layout_3).setVisibility(View.VISIBLE);
+//                    holder.setText(R.id.tv_san, models.get(i).getSchool_id());
+//                    break;
                 default:
                     break;
             }
