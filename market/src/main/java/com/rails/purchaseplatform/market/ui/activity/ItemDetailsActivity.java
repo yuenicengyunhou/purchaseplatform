89b03be67d21d813ee0a3e3a66b1872a74001ab1
ZhangXiaofu <1039999302@qq.com>
2021-03-20 18:55:54 +0800

    TabLayout+ViewPager+Fragment

--- market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ItemDetailsActivity.java
+++ market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ItemDetailsActivity.java
@@ -1,12 +1,11 @@
 package com.rails.purchaseplatform.market.ui.activity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
 import com.rails.lib_data.bean.RecommendItemsBean;
 import com.rails.lib_data.contract.RecommendItemsContract;
 import com.rails.lib_data.contract.RecommendItemsPresenterImpl;
@@ -14,13 +13,18 @@ import com.rails.purchaseplatform.common.widget.BaseRecyclerView;
 import com.rails.purchaseplatform.framwork.base.BaseErrorActivity;
 import com.rails.purchaseplatform.market.adapter.RecommendItemsRecyclerAdapter;
 import com.rails.purchaseplatform.market.databinding.ActivityItemDetailsBinding;
+import com.rails.purchaseplatform.market.ui.fragment.TabFragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ItemDetailsActivity extends BaseErrorActivity<ActivityItemDetailsBinding> implements RecommendItemsContract.RecommendItemsView {
 
     private RecommendItemsRecyclerAdapter recommendItemsRecyclerAdapter;
     private RecommendItemsContract.RecommendItemsPresenter recommendItemsPresenter;
+
+    private String[] tabTitles = {"商品信息", "包装清单", "售后服务", "推荐单位"};
+    private List<TabFragment> tabFragments = new ArrayList<>();
 
 
     @Override
@@ -30,6 +34,17 @@ public class ItemDetailsActivity extends BaseErrorActivity<ActivityItemDetailsBi
         binding.recyclerRecommendItems.setLayoutManager(BaseRecyclerView.GRID, RecyclerView.VERTICAL, false, 3);
         binding.recyclerRecommendItems.setAdapter(recommendItemsRecyclerAdapter);
         recommendItemsPresenter.getRecommendItems(false, 1);
+
+        TabLayout tabLayout = binding.tabDetails;
+        ViewPager2 viewPager2 = binding.pagerDetails;
+        for (int i = 0; i < tabTitles.length; i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(tabTitles[i]));
+            tabFragments.add(TabFragment.newInstance(tabTitles[i]));
+        }
+
+
+//        binding.tabDetails
+//        binding.pagerDetails
     }
 
     @Override
---
+++ market/src/main/java/com/rails/purchaseplatform/market/ui/fragment/TabFragment.java
@@ -0,0 +1,58 @@
+package com.rails.purchaseplatform.market.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.rails.purchaseplatform.market.R;
+
+public class TabFragment extends Fragment {
+
+    private WebSettings mWebSettings;
+
+    public static TabFragment newInstance(String label) {
+        Bundle args = new Bundle();
+        args.putString("label", label);
+        TabFragment fragment = new TabFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        String label = getArguments().getString("label");
+        WebView view = getView().findViewById(R.id.web_view);
+        view.setBackgroundColor(android.R.color.darker_gray);
+
+        mWebSettings = view.getSettings();
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        mWebSettings.setJavaScriptEnabled(true);//是否允许JavaScript脚本运行，默认为false。设置true时，会提醒可能造成XSS漏洞
+        mWebSettings.setSupportZoom(true);//是否可以缩放，默认true
+        mWebSettings.setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        mWebSettings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        mWebSettings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        mWebSettings.setAppCacheEnabled(true);//是否使用缓存
+        mWebSettings.setDomStorageEnabled(true);//开启本地DOM存储
+        mWebSettings.setLoadsImagesAutomatically(true); // 加载图片
+        mWebSettings.setMediaPlaybackRequiresUserGesture(false);//播放音频，多媒体需要用户手动？设置为false为可自动播放
+
+//        view.loadUrl("DETAILS_WEB_URL");
+//        view.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+    }
+}
