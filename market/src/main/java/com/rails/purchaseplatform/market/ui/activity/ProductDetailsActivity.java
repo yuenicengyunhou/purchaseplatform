dc20dac58effe760c52dd5dfa3bb6dcd202d3681
ZhangXiaofu <zhangsep24@163.com>
2021-03-23 18:44:01 +0800

    TabLayout和NestedScrollView联动

--- market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ProductDetailsActivity.java
+++ market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ProductDetailsActivity.java
@@ -2,32 +2,26 @@ package com.rails.purchaseplatform.market.ui.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
-import com.orhanobut.logger.Logger;
 import com.rails.lib_data.bean.RecommendItemsBean;
 import com.rails.lib_data.contract.RecommendItemsContract;
 import com.rails.lib_data.contract.RecommendItemsPresenterImpl;
 import com.rails.purchaseplatform.common.widget.BaseRecyclerView;
-import com.rails.purchaseplatform.common.widget.RatioImage;
 import com.rails.purchaseplatform.framwork.base.BaseErrorActivity;
 import com.rails.purchaseplatform.framwork.utils.ScreenSizeUtil;
-import com.rails.purchaseplatform.market.R;
 import com.rails.purchaseplatform.market.adapter.RecommendItemsRecyclerAdapter;
-import com.rails.purchaseplatform.market.adapter.SectionsPagerAdapter;
 import com.rails.purchaseplatform.market.databinding.ActivityProductDetailsBinding;
-import com.rails.purchaseplatform.market.ui.fragment.PlaceholderFragment;
 import com.rails.purchaseplatform.market.web.PackageListPage4Js;
 import com.rails.purchaseplatform.market.web.ProductInfoPage4Js;
 import com.rails.purchaseplatform.market.web.RecommendPage4Js;
@@ -35,7 +29,7 @@ import com.rails.purchaseplatform.market.web.ServicePage4Js;
 
 import java.util.ArrayList;
 
-public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDetailsBinding> implements RecommendItemsContract.RecommendItemsView, NestedScrollView.OnScrollChangeListener {
+public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDetailsBinding> implements RecommendItemsContract.RecommendItemsView {
     final private String TAG = ProductDetailsActivity.class.getSimpleName();
 
     private RecommendItemsRecyclerAdapter recommendItemsRecyclerAdapter;
@@ -55,7 +49,7 @@ public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDet
     };
 
 
-    @SuppressLint("JavascriptInterface")
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void initialize(Bundle bundle) {
         recommendItemsRecyclerAdapter = new RecommendItemsRecyclerAdapter(this);
@@ -65,6 +59,73 @@ public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDet
         recommendItemsPresenter.getRecommendItems(false, 1);
 
 
+        loadWebView();
+
+        int px = (int) ScreenSizeUtil.dp2px(this, 80);
+        float px2 = ScreenSizeUtil.dp2px(this, 240);
+
+        /**
+         * 监视TabLayout标签事件，使NestedScrollView滚动到相应的位置
+         */
+        binding.tabDetails.setTransitionAlpha(0);
+        binding.tabDetails.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (binding.tabDetails.getTabAt(0).equals(tab)) {
+                    binding.nestedScrollView.scrollTo(0, 0);
+                } else if (binding.tabDetails.getTabAt(1).equals(tab)) {
+                    binding.nestedScrollView.scrollBy(0, getCurrentPositionY(ProductDetailsActivity.this, binding.webPackageList) - px);
+                } else if (binding.tabDetails.getTabAt(2).equals(tab)) {
+                    binding.nestedScrollView.scrollTo(0, getCurrentPositionY(ProductDetailsActivity.this, binding.webService) - px);
+                } else if (binding.tabDetails.getTabAt(3).equals(tab)) {
+                    binding.nestedScrollView.scrollTo(0, getCurrentPositionY(ProductDetailsActivity.this, binding.webRecommend) - px);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        /**
+         * 监听NestedScrollView滚动事件设置TabLayout透明度和Tab标签
+         */
+        binding.nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+
+                float llFlagPosition = getCurrentPositionY(ProductDetailsActivity.this, binding.llFlag) - (float) px;
+
+                binding.tabDetails.setTransitionAlpha(1 - (llFlagPosition / px2));
+
+                int productInfoPosition = getCurrentPositionY(ProductDetailsActivity.this, binding.webProductInfo) - px;
+                int packageListPosition = getCurrentPositionY(ProductDetailsActivity.this, binding.webPackageList) - px;
+                int servicePosition = getCurrentPositionY(ProductDetailsActivity.this, binding.webService) - px;
+                int recommendPosition = getCurrentPositionY(ProductDetailsActivity.this, binding.webRecommend) - px;
+
+                if (packageListPosition <= 0 && servicePosition > 0) {
+                    binding.tabDetails.selectTab(binding.tabDetails.getTabAt(1));
+                } else if (servicePosition <= 0 && recommendPosition > 0) {
+                    binding.tabDetails.selectTab(binding.tabDetails.getTabAt(2));
+                } else if (recommendPosition <= 0) {
+                    binding.tabDetails.selectTab(binding.tabDetails.getTabAt(3));
+                }
+
+            }
+        });
+    }
+
+    /**
+     * 加载4个WebView
+     */
+    @SuppressLint("JavascriptInterface")
+    private void loadWebView() {
         WebView[] WEB_VIEWS = {
                 binding.webProductInfo,
                 binding.webPackageList,
@@ -91,52 +152,6 @@ public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDet
             WEB_VIEWS[i].loadUrl(TAB_URLS[i]);
             WEB_VIEWS[i].addJavascriptInterface(OBJECTS[i], FLAGS[i]);
         }
-
-
-        int width = View.MeasureSpec.makeMeasureSpec(0,
-                View.MeasureSpec.UNSPECIFIED);
-        int height = View.MeasureSpec.makeMeasureSpec(0,
-                View.MeasureSpec.UNSPECIFIED);
-        binding.riItemPhoto.measure(width, height);
-        Log.d(TAG, "getMeasuredWidth = " + binding.riItemPhoto.getMeasuredWidth());
-        Log.d(TAG, "getMeasuredHeight = " + binding.riItemPhoto.getMeasuredHeight());
-
-//        binding.tabDetails.selectTab(binding.tabDetails.getTabAt(3));
-
-        binding.tabDetails.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                if (binding.tabDetails.getTabAt(0).equals(tab)) {
-                    binding.nestedScrollView.scrollTo(0, 200);
-                } else if (binding.tabDetails.getTabAt(1).equals(tab)) {
-                    binding.nestedScrollView.scrollTo(0, 400);
-                } else if (binding.tabDetails.getTabAt(2).equals(tab)) {
-                    binding.nestedScrollView.scrollTo(0, 600);
-                } else if (binding.tabDetails.getTabAt(3).equals(tab)) {
-                    binding.nestedScrollView.scrollTo(0, 800);
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-        binding.nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                Log.d(TAG, "scrollX = " + scrollX + ", scrollY = " + scrollY + ", oldScrollX = " + oldScrollX + ", oldScrollY = " + oldScrollY);
-                binding.tabDetails.selectTab(binding.tabDetails.getTabAt(2));
-                getLocalVisibleRect(ProductDetailsActivity.this, binding.webProductInfo);
-            }
-        });
     }
 
     @Override
@@ -165,16 +180,11 @@ public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDet
         recommendItemsRecyclerAdapter.update(recommendItemsBeans, false);
     }
 
-    @Override
-    public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-        Log.d("HELLO = scrollX", String.valueOf(scrollX));
-        Log.d("HELLO = scrollY", String.valueOf(scrollY));
-        Log.d("HELLO = oldScrollX", String.valueOf(oldScrollX));
-        Log.d("HELLO = oldScrollY", String.valueOf(oldScrollY));
-
-
-    }
-
+    /**
+     * WebView属性设置
+     *
+     * @param view WebView
+     */
     private void setWeb(WebView view) {
         WebSettings webSettings = view.getSettings();
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
@@ -194,6 +204,20 @@ public class ProductDetailsActivity extends BaseErrorActivity<ActivityProductDet
         int[] location = new int[2];
         view.getLocationOnScreen(location);
         view.setTag(location[1]);//存储y方向的位置
+        Log.d("HELLO -> LOCATION-Y = ", "" + location[1]);
+    }
+
+    /**
+     * 获取View与Window的距离
+     *
+     * @param context Context
+     * @param view    需要测量的View
+     * @return 返回View左上角与Window左上角的Y方向的距离
+     */
+    public int getCurrentPositionY(Context context, View view) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        return location[1];
     }
 
 
