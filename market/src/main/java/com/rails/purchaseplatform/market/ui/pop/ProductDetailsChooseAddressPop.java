55dd3548799e79c3ad96d3d6729d4f1e96814628
ZhangXiaofu <zhangsep24@163.com>
2021-04-01 11:08:51 +0800

    -1- 三个弹窗页面

---
+++ market/src/main/java/com/rails/purchaseplatform/market/ui/pop/ProductDetailsChooseAddressPop.java
@@ -0,0 +1,13 @@
+package com.rails.purchaseplatform.market.ui.pop;
+
+import android.os.Bundle;
+
+import com.rails.purchaseplatform.framwork.base.BasePop;
+import com.rails.purchaseplatform.market.databinding.PopProductDetailsChooseAddressBinding;
+
+public class ProductDetailsChooseAddressPop extends BasePop<PopProductDetailsChooseAddressBinding> {
+    @Override
+    protected void initialize(Bundle bundle) {
+
+    }
+}
