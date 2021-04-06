5450cf3722e9c8706fd58f93e51c3bff284dbeea
ZhangXiaofu <zhangsep24@163.com>
2021-04-06 17:11:25 +0800

    -1- 限制ViewPager滚动

--- common/src/main/java/com/rails/purchaseplatform/common/widget/ScrollControlledViewPager.java
+++ common/src/main/java/com/rails/purchaseplatform/common/widget/ScrollControlledViewPager.java
@@ -1,4 +1,36 @@
 package com.rails.purchaseplatform.common.widget;
 
-public class ScrollControledViewPager {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class ScrollControlledViewPager extends ViewPager {
+
+    private boolean isPagingEnabled = true;
+
+    public ScrollControlledViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public ScrollControlledViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onInterceptTouchEvent(event);
+    }
+
+    public void setPagingEnabled(boolean b) {
+        this.isPagingEnabled = b;
+    }
 }
