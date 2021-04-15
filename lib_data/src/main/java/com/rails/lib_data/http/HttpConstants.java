75a42fc5c5ff373778dbad095de73b814152d448
ZhangXiaofu <1039999302@qq.com>
2021-04-15 19:55:16 +0800

    -1- 地址

--- lib_data/src/main/java/com/rails/lib_data/http/HttpConstants.java
+++ lib_data/src/main/java/com/rails/lib_data/http/HttpConstants.java
@@ -6,10 +6,14 @@ package com.rails.lib_data.http;
 public class HttpConstants {
 
 
-    public static String SHOP_URL = "https://shop.rails.cn/proxy/";
-//    public static String SHOP_URL = "http://gateway.shop-dev1.rails.cn:80/app-item-service/app/v1/";
+    final public static String SHOP_URL = "https://shop.rails.cn/proxy/";
 
-    public static String PLATFORM_URL = "https://crmall.rails.cn/proxy/passport/";
-//    public static String PLATFORM_URL = "http://172.28.22.72:8080/";
+    /**
+     * Swagger给的新的地址
+     */
+//    final public static String SHOP_URL = "http://gateway.shop-dev1.rails.cn:80/app-item-service/app/v1/";
+
+    final public static String PLATFORM_URL = "https://crmall.rails.cn/proxy/passport/";
+//   final public static String PLATFORM_URL = "http://172.28.22.72:8080/";
 
 }
