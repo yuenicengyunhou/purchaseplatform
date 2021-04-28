600fc513a8706a9fccc49b9f0cb12edada0c740e
ZhangXiaofu <1039999302@qq.com>
2021-04-28 16:33:48 +0800

    -1- arrtFlag

--- lib_data/src/main/java/com/rails/lib_data/bean/forAppShow/SearchFilterValue.java
+++ lib_data/src/main/java/com/rails/lib_data/bean/forAppShow/SearchFilterValue.java
@@ -3,6 +3,8 @@ package com.rails.lib_data.bean.forAppShow;
 public class SearchFilterValue {
     private String valueName;
     private String valueId;
+    private boolean isSelect = false;
+    private int attrFlag = -1; // 0-brand，1-cid, 2-categoryAttrValue, 3-expandAttrValue
 
 
     // =============================================================================
@@ -22,5 +24,21 @@ public class SearchFilterValue {
 
     public void setValueId(String valueId) {
         this.valueId = valueId;
+    }
+
+    public boolean isSelect() {
+        return isSelect;
+    }
+
+    public void setSelect(boolean select) {
+        isSelect = select;
+    }
+
+    public int getAttrFlag() {
+        return attrFlag;
+    }
+
+    public void setAttrFlag(int attrFlag) {
+        this.attrFlag = attrFlag;
     }
 }
