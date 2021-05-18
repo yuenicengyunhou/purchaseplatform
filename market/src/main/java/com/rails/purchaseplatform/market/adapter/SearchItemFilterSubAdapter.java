3a4f50f0ad46d9d80d709220a5292daa9d1637fb
ZhangXiaofu <1039999302@qq.com>
2021-05-18 10:55:10 +0800

    -1- Bugfixed ID=7944 店铺搜索，连续点击不同的收货区域，会选中2条

--- market/src/main/java/com/rails/purchaseplatform/market/adapter/SearchItemFilterSubAdapter.java
+++ market/src/main/java/com/rails/purchaseplatform/market/adapter/SearchItemFilterSubAdapter.java
@@ -1,20 +1,31 @@
 package com.rails.purchaseplatform.market.adapter;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
 import com.rails.lib_data.bean.forAppShow.SearchFilterValue;
 import com.rails.purchaseplatform.framwork.adapter.BaseAbsAdapter;
+import com.rails.purchaseplatform.framwork.loading.LoadingDialog;
 import com.rails.purchaseplatform.market.R;
 
 public class SearchItemFilterSubAdapter extends BaseAbsAdapter<SearchFilterValue> {
+
+    private Context mContext;
+
+    private static int COUNTING = 1;
+    private int COUNT_NUM = 60;
+
+
     private boolean isMultiSelect;
 
     public SearchItemFilterSubAdapter(Context context, boolean isMultiSelect) {
         super(context);
+        this.mContext = context;
         this.isMultiSelect = isMultiSelect;
     }
 
@@ -33,6 +44,11 @@ public class SearchItemFilterSubAdapter extends BaseAbsAdapter<SearchFilterValue
         boolean select = mDataSource.get(position).isSelect();
         holder.rbTag.setSelected(select);
         holder.rbTag.setOnClickListener(v -> {
+            LoadingDialog mLoadingDialog = new LoadingDialog.Builder(mContext).setMessage("Loading").create();
+            mLoadingDialog.setCancelable(true);
+            LoadingHandler mHandler = new LoadingHandler(mLoadingDialog);
+            mLoadingDialog.show();
+
             if (!isMultiSelect) {
                 for (SearchFilterValue value : mDataSource) {
                     if (value.isSelect()) {
@@ -42,6 +58,11 @@ public class SearchItemFilterSubAdapter extends BaseAbsAdapter<SearchFilterValue
             }
             mDataSource.get(position).setSelect(!select);
             notifyDataSetChanged();
+
+            Message message = new Message();
+            message.what = COUNTING;
+            message.obj = COUNT_NUM;
+            mHandler.sendMessageDelayed(message, 500);
         });
 
         return convertView;
@@ -54,6 +75,24 @@ public class SearchItemFilterSubAdapter extends BaseAbsAdapter<SearchFilterValue
         ViewHolder(View view) {
             rbTag = view.findViewById(R.id.rb_tag);
             rbTag.setGravity(Gravity.CENTER);
+        }
+    }
+
+
+    private static class LoadingHandler extends Handler {
+        private final LoadingDialog mDialog;
+
+        public LoadingHandler(LoadingDialog dialog) {
+            mDialog = dialog;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == COUNTING) {
+                if (mDialog.isShowing())
+                    mDialog.dismiss();
+            }
         }
     }
 }
