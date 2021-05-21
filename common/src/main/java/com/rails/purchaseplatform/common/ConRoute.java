c30f503dbe76de787d8b327338acfd6149f6d3f1
王超 <sk_comic@163.com>
2021-05-21 19:06:56 +0800

    订单权限

--- common/src/main/java/com/rails/purchaseplatform/common/ConRoute.java
+++ common/src/main/java/com/rails/purchaseplatform/common/ConRoute.java
@@ -123,10 +123,10 @@ public class ConRoute {
 //        public final static String BASEURL = "http://172.28.22.140:3000/purchase-android-web/#/";
 
         //dev地址
-        public final static String BASEURL = "http://crmall.rails.cn/purchase-android-web/#/";
+//        public final static String BASEURL = "http://crmall.rails.cn/purchase-android-web/#/";
 
-        // SongYanxia - local
-//        public final static String BASEURL = "http://172.30.1.82:3000/purchase-android-web/";
+        // 生产环境
+        public final static String BASEURL = "http://cg.95306.cn/purchase-android-web/#/";
 
 
         public final static String HOME = BASEURL + "home";
