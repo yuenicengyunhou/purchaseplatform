72bbed33c9ed676e76cd26a285bace9d8600c437
ZhangXiaofu <1039999302@qq.com>
2021-07-08 09:26:42 +0800

    -1- String format.

--- lib_data/src/main/java/com/rails/lib_data/model/LoginModel.java
+++ lib_data/src/main/java/com/rails/lib_data/model/LoginModel.java
@@ -43,9 +43,9 @@ public class LoginModel {
         params.put("mobilePhoneCode", code);
         params.put("isWeakPwd", "0");
         params.put("returnUrl", "");
-        params.put("loginDevice", "android" + Build.MODEL); // 登录设备 固定Android
-        params.put("osVersionNumber", "android" + Build.VERSION.RELEASE);
-        params.put("loginMedia", "api" + Build.VERSION.SDK_INT); // 设备版本 or APP版本
+        params.put("loginDevice", "android@" + Build.BRAND + "#" + Build.MODEL);// 登录设备 固定Android
+        params.put("osVersionNumber", "android@version" + Build.VERSION.RELEASE);
+        params.put("loginMedia", "android@api" + Build.VERSION.SDK_INT); // 设备版本 or APP版本
         if (location != null) {
             params.put("longitude", String.format("%6f", location[0]));
             params.put("latitude", String.format("%6f", location[1]));
@@ -185,9 +185,9 @@ public class LoginModel {
         params.put("userAccount", account); // 账号
         params.put("userPassword", password); // 密码
         params.put("rndCode", rndCode); // 随机码
-        params.put("loginDevice", "android" + Build.MODEL); // 登录设备 固定Android
-        params.put("osVersionNumber", "android" + Build.VERSION.RELEASE);
-        params.put("loginMedia", "api" + Build.VERSION.SDK_INT); // 设备版本 or APP版本
+        params.put("loginDevice", "android@" + Build.BRAND + "#" + Build.MODEL); // 登录设备 固定Android
+        params.put("osVersionNumber", "android@version" + Build.VERSION.RELEASE);
+        params.put("loginMedia", "android@api" + Build.VERSION.SDK_INT); // 设备版本 or APP版本
         params.put("requestFlag", requestFlag); // 这是啥？ 是randInit接口返回的字符串？
         params.put("isWeakPwd", "0"); // 不是弱密码 固定0
         params.put("returnUrl", ""); // 返回的url 不需要
