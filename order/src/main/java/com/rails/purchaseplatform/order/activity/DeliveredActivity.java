ed98e9dc64fd2f8fe9ffca2b7f685d2533f46772
wangqi <1751539011@qq.com>
2021-08-13 15:00:17 +0800

    地址搜索加回来了

--- order/src/main/java/com/rails/purchaseplatform/order/activity/DeliveredActivity.java
+++ order/src/main/java/com/rails/purchaseplatform/order/activity/DeliveredActivity.java
@@ -16,8 +16,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.liulishuo.filedownloader.BaseDownloadTask;
-import com.liulishuo.filedownloader.FileDownloadListener;
-import com.liulishuo.filedownloader.FileDownloadQueueSet;
 import com.liulishuo.filedownloader.FileDownloadSampleListener;
 import com.liulishuo.filedownloader.FileDownloader;
 import com.liulishuo.filedownloader.util.FileDownloadUtils;
@@ -41,7 +39,6 @@ import java.io.File;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 @Route(path = ConRoute.ORDER.ORDER_DELIVER)
 public class DeliveredActivity extends BaseErrorActivity<ActivityDeliveredBinding> implements OrderContract.OrderView {
@@ -52,9 +49,9 @@ public class DeliveredActivity extends BaseErrorActivity<ActivityDeliveredBindin
 
     //下载
     private int currentIndex = -1;//默认为-1,为-1时没有下载任务，不为-1时等待下载完成
-    private final List<BaseDownloadTask> tasks = new ArrayList<>();
-    private HashMap<Integer, String> urlMap = new HashMap<>();
-    private FileDownloadQueueSet queueSet;
+//    private final List<BaseDownloadTask> tasks = new ArrayList<>();
+//    private HashMap<Integer, String> urlMap = new HashMap<>();
+//    private FileDownloadQueueSet queueSet;
 
     //刷新下载状态
     private final UIHandler uiHandler = new UIHandler(this);
@@ -285,149 +282,149 @@ public class DeliveredActivity extends BaseErrorActivity<ActivityDeliveredBindin
 //    }
 
     // 多任务下载
-    private FileDownloadListener downloadListener;
+//    private FileDownloadListener downloadListener;
+//
+//    public FileDownloadListener createLis() {
+//        return new FileDownloadSampleListener() {
+//            @Override
+//            protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "pending taskId:" + task.getId() + ",fileName:" + task.getFilename() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes);
+//
+//            }
+//
+//            @Override
+//            protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "progress taskId:" + task.getId() + ",fileName:" + task.getFilename() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes + ",speed:" + task.getSpeed());
+//            }
+//
+//            @Override
+//            protected void blockComplete(BaseDownloadTask task) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "blockComplete taskId:" + task.getId() + ",filePath:" + task.getPath() + ",fileName:" + task.getFilename() + ",speed:" + task.getSpeed() + ",isReuse:" + task.reuse());
+//            }
+//
+//            @Override
+//            protected void completed(BaseDownloadTask task) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "completed taskId:" + task.getId() + ",isReuse:" + task.reuse());
+//            }
+//
+//            @Override
+//            protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "paused taskId:" + task.getId() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes);
+//            }
+//
+//            @Override
+//            protected void error(BaseDownloadTask task, Throwable e) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "error taskId:" + task.getId() + ",e:" + e.getLocalizedMessage());
+//            }
+//
+//            @Override
+//            protected void warn(BaseDownloadTask task) {
+//                if (task.getListener() != downloadListener) {
+//                    return;
+//                }
+//                Log.d("feifei", "warn taskId:" + task.getId());
+//            }
+//        };
+//    }
 
-    public FileDownloadListener createLis() {
-        return new FileDownloadSampleListener() {
-            @Override
-            protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "pending taskId:" + task.getId() + ",fileName:" + task.getFilename() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes);
+//    private void addTasks(int position, String url, int positionPlus, int downloadState) {
+//        adapter.notifyDownloadState(position, 1, 0);
+//        String extension = url.substring(url.lastIndexOf("."));
+//        String mFilePath = mSinglePath + orderNo + "_" + positionPlus + extension;
+//        urlMap.put(position, url);
+//        BaseDownloadTask task = FileDownloader.getImpl().create(url).setPath(mFilePath, false);
+//        tasks.add(task);
+//        start_multi();
+//    }
 
-            }
+//    public void start_multi() {
+//
+//        downloadListener = createLis();
+//        //(1) 创建 FileDownloadQueueSet
+//        if (null == queueSet) {
+//            queueSet = new FileDownloadQueueSet(downloadListener);
+//            //(2) 创建Task 队列
+//
+//
+////        tasks.add(task1);
+////        BaseDownloadTask task2 = FileDownloader.getImpl().create(BIG_FILE_URLS[4]).setPath(mSaveFolder,true);
+////        tasks.add(task2);
+//
+//            //(3) 设置参数
+//
+//            // 每个任务的进度 无回调
+//            //queueSet.disableCallbackProgressTimes();
+//            // do not want each task's download progress's callback,we just consider which task will completed.
+//
+//            queueSet.setCallbackProgressTimes(100);
+//            queueSet.setCallbackProgressMinInterval(100);
+//            //失败 重试次数
+//            queueSet.setAutoRetryTimes(3);
+//
+//            //避免掉帧
+//            FileDownloader.enableAvoidDropFrame();
+//
+//        }
+//        //(4)串行下载
+//        queueSet.downloadSequentially(tasks);
+//
+//        //(5)任务启动
+//        queueSet.start();
+//    }
 
-            @Override
-            protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "progress taskId:" + task.getId() + ",fileName:" + task.getFilename() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes + ",speed:" + task.getSpeed());
-            }
+//    public void stop_multi() {
+//        FileDownloader.getImpl().pause(downloadListener);
+//    }
 
-            @Override
-            protected void blockComplete(BaseDownloadTask task) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "blockComplete taskId:" + task.getId() + ",filePath:" + task.getPath() + ",fileName:" + task.getFilename() + ",speed:" + task.getSpeed() + ",isReuse:" + task.reuse());
-            }
-
-            @Override
-            protected void completed(BaseDownloadTask task) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "completed taskId:" + task.getId() + ",isReuse:" + task.reuse());
-            }
-
-            @Override
-            protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "paused taskId:" + task.getId() + ",soFarBytes:" + soFarBytes + ",totalBytes:" + totalBytes + ",percent:" + soFarBytes * 1.0 / totalBytes);
-            }
-
-            @Override
-            protected void error(BaseDownloadTask task, Throwable e) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "error taskId:" + task.getId() + ",e:" + e.getLocalizedMessage());
-            }
-
-            @Override
-            protected void warn(BaseDownloadTask task) {
-                if (task.getListener() != downloadListener) {
-                    return;
-                }
-                Log.d("feifei", "warn taskId:" + task.getId());
-            }
-        };
-    }
-
-    private void addTasks(int position, String url, int positionPlus, int downloadState) {
-        adapter.notifyDownloadState(position, 1, 0);
-        String extension = url.substring(url.lastIndexOf("."));
-        String mFilePath = mSinglePath + orderNo + "_" + positionPlus + extension;
-        urlMap.put(position, url);
-        BaseDownloadTask task = FileDownloader.getImpl().create(url).setPath(mFilePath, false);
-        tasks.add(task);
-        start_multi();
-    }
-
-    public void start_multi() {
-
-        downloadListener = createLis();
-        //(1) 创建 FileDownloadQueueSet
-        if (null == queueSet) {
-            queueSet = new FileDownloadQueueSet(downloadListener);
-            //(2) 创建Task 队列
-
-
-//        tasks.add(task1);
-//        BaseDownloadTask task2 = FileDownloader.getImpl().create(BIG_FILE_URLS[4]).setPath(mSaveFolder,true);
-//        tasks.add(task2);
-
-            //(3) 设置参数
-
-            // 每个任务的进度 无回调
-            //queueSet.disableCallbackProgressTimes();
-            // do not want each task's download progress's callback,we just consider which task will completed.
-
-            queueSet.setCallbackProgressTimes(100);
-            queueSet.setCallbackProgressMinInterval(100);
-            //失败 重试次数
-            queueSet.setAutoRetryTimes(3);
-
-            //避免掉帧
-            FileDownloader.enableAvoidDropFrame();
-
-        }
-        //(4)串行下载
-        queueSet.downloadSequentially(tasks);
-
-        //(5)任务启动
-        queueSet.start();
-    }
-
-    public void stop_multi() {
-        FileDownloader.getImpl().pause(downloadListener);
-    }
-
-    public void deleteAllFile() {
-
-        //清除所有的下载任务
-        FileDownloader.getImpl().clearAllTaskData();
-
-        //清除所有下载的文件
-        int count = 0;
-        File file = new File(FileDownloadUtils.getDefaultSaveRootPath());
-        do {
-            if (!file.exists()) {
-                break;
-            }
-
-            if (!file.isDirectory()) {
-                break;
-            }
-
-            File[] files = file.listFiles();
-
-            if (files == null) {
-                break;
-            }
-
-            for (File file1 : files) {
-                count++;
-                file1.delete();
-            }
-
-        } while (false);
-
-    }
+//    public void deleteAllFile() {
+//
+//        //清除所有的下载任务
+//        FileDownloader.getImpl().clearAllTaskData();
+//
+//        //清除所有下载的文件
+//        int count = 0;
+//        File file = new File(FileDownloadUtils.getDefaultSaveRootPath());
+//        do {
+//            if (!file.exists()) {
+//                break;
+//            }
+//
+//            if (!file.isDirectory()) {
+//                break;
+//            }
+//
+//            File[] files = file.listFiles();
+//
+//            if (files == null) {
+//                break;
+//            }
+//
+//            for (File file1 : files) {
+//                count++;
+//                file1.delete();
+//            }
+//
+//        } while (false);
+//
+//    }
 
     @Override
     public void onBackPressed() {
