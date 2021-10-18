a45818f94a7eb80135e4495db5601ddca9cc3454
王超 <sk_comic@163.com>
2021-10-18 17:26:57 +0800

    分类折叠

--- market/src/main/java/com/rails/purchaseplatform/market/adapter/CategoryAdapter.java
+++ market/src/main/java/com/rails/purchaseplatform/market/adapter/CategoryAdapter.java
@@ -1,6 +1,8 @@
 package com.rails.purchaseplatform.market.adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.SparseBooleanArray;
 
 import com.rails.lib_data.bean.CategoryBean;
 import com.rails.lib_data.bean.CategorySubBean;
@@ -10,6 +12,8 @@ import com.rails.purchaseplatform.framwork.adapter.listener.PositionListener;
 import com.rails.purchaseplatform.market.R;
 import com.rails.purchaseplatform.market.databinding.ItemCategoryBinding;
 import com.rails.purchaseplatform.market.widget.CategoryDecoration;
+
+import java.util.ArrayList;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,10 +25,13 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 public class CategoryAdapter extends BaseRecyclerAdapter<CategoryBean, ItemCategoryBinding> {
 
+    private SparseBooleanArray sels;
 
     public CategoryAdapter(Context context) {
         super(context);
+        sels = new SparseBooleanArray();
     }
+
 
     @Override
     protected int getContentID() {
@@ -32,16 +39,26 @@ public class CategoryAdapter extends BaseRecyclerAdapter<CategoryBean, ItemCateg
     }
 
     @Override
-    protected void onBindItem(ItemCategoryBinding binding, CategoryBean categoryBean, int position) {
+    protected void onBindItem(ItemCategoryBinding binding, CategoryBean categoryBean, int item) {
         binding.setCategory(categoryBean);
         CategorySubAdapter adapter = new CategorySubAdapter(mContext);
         binding.recycler.setAdapter(adapter);
-        adapter.update(categoryBean.getThirdPlatformCategoryList(), true);
+
+
+        ArrayList<CategorySubBean> categorySubBeans = categoryBean.getThirdPlatformCategoryList();
+        adapter.updateData(getChilds(categorySubBeans, sels.get(item)), true, !sels.get(item));
         adapter.setListener(new PositionListener<CategorySubBean>() {
             @Override
             public void onPosition(CategorySubBean bean, int position) {
-                if (positionListener != null)
-                    positionListener.onPosition(bean, position);
+                if (positionListener != null) {
+                    if (!TextUtils.isEmpty(bean.getFcid())) {
+                        positionListener.onPosition(bean, position);
+                    } else {
+                        adapter.updateData(getChilds(categorySubBeans, !sels.get(item)), true, sels.get(item));
+                        sels.put(item, !sels.get(item));
+                    }
+                }
+
             }
         });
 
@@ -54,4 +71,34 @@ public class CategoryAdapter extends BaseRecyclerAdapter<CategoryBean, ItemCateg
         binding.recycler.setLayoutManager(BaseRecyclerView.GRID, RecyclerView.VERTICAL, false, 3);
         binding.recycler.addItemDecoration(new CategoryDecoration(mContext));
     }
+
+
+    /**
+     * @param totalBeans
+     * @param isExpand   是否展开
+     * @return
+     */
+    private ArrayList<CategorySubBean> getChilds(ArrayList<CategorySubBean> totalBeans, boolean isExpand) {
+        ArrayList<CategorySubBean> childBeans = new ArrayList<>();
+        if (totalBeans == null) {
+            return childBeans;
+        }
+        if (totalBeans.size() > 9) {
+            if (isExpand) {
+                childBeans.addAll(totalBeans);
+                childBeans.add(new CategorySubBean("收起", R.drawable.ic_category_up));
+            } else {
+                for (int i = 0; i < 8; i++) {
+                    childBeans.add(totalBeans.get(i));
+                }
+                childBeans.add(new CategorySubBean("展开", R.drawable.ic_category_down));
+            }
+
+        } else {
+            childBeans.addAll(totalBeans);
+        }
+        return childBeans;
+    }
+
+
 }
