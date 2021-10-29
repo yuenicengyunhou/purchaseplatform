ce04b68bf8c6881a062d9410d7336bdb450b9115
王超 <sk_comic@163.com>
2021-10-29 17:22:13 +0800

    分类折叠

--- market/src/main/java/com/rails/purchaseplatform/market/adapter/CategorySubAdapter.java
+++ market/src/main/java/com/rails/purchaseplatform/market/adapter/CategorySubAdapter.java
@@ -1,6 +1,8 @@
 package com.rails.purchaseplatform.market.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.rails.lib_data.bean.CategorySubBean;
@@ -8,11 +10,16 @@ import com.rails.purchaseplatform.framwork.adapter.BaseRecyclerAdapter;
 import com.rails.purchaseplatform.market.R;
 import com.rails.purchaseplatform.market.databinding.ItemCategorySub2Binding;
 
+import java.util.ArrayList;
+
 /**
  * @author： sk_comic@163.com
  * @date: 2021/3/2
  */
 public class CategorySubAdapter extends BaseRecyclerAdapter<CategorySubBean, ItemCategorySub2Binding> {
+
+    private boolean isExpand = false;
+
     public CategorySubAdapter(Context context) {
         super(context);
     }
@@ -26,6 +33,19 @@ public class CategorySubAdapter extends BaseRecyclerAdapter<CategorySubBean, Ite
     protected void onBindItem(ItemCategorySub2Binding binding, CategorySubBean categoryBean, int position) {
         binding.setSub(categoryBean);
 
+        String fid = categoryBean.getFcid();
+        if (TextUtils.isEmpty(fid)) {
+            Drawable right;
+            if (isExpand) {
+                right = mContext.getResources().getDrawable(R.drawable.ic_category_down);
+            } else {
+                right = mContext.getResources().getDrawable(R.drawable.ic_category_up);
+            }
+            binding.tvName.setCompoundDrawablesWithIntrinsicBounds(null, null, right, null);
+        }else{
+            binding.tvName.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+        }
+
         binding.getRoot().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,4 +56,19 @@ public class CategorySubAdapter extends BaseRecyclerAdapter<CategorySubBean, Ite
         });
     }
 
+
+    public void updateData(ArrayList<CategorySubBean> itemDatas, boolean isClear, boolean isExpand) {
+        this.isExpand = isExpand;
+        if (itemDatas == null)
+            return;
+        if (isClear)
+            this.mDataSource.clear();
+
+        if (itemDatas.isEmpty()) {
+            this.notifyDataSetChanged();
+            return;
+        }
+        this.mDataSource.addAll(itemDatas);
+        this.notifyDataSetChanged();
+    }
 }
