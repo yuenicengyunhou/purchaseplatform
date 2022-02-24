349af0c0ece7a9b4ea7065893dd0465a120cdb58
ZhangXiaofu <1039999302@qq.com>
2022-02-24 17:24:52 +0800

    TITLE: ImageZoom -1- 查看大图页面状态栏颜色适配

--- market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ImageZoomActivity.java
+++ market/src/main/java/com/rails/purchaseplatform/market/ui/activity/ImageZoomActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -18,10 +19,13 @@ import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.rails.purchaseplatform.common.ConRoute;
 import com.rails.purchaseplatform.common.base.BaseErrorActivity;
 import com.rails.purchaseplatform.common.utils.StatusBarCompat;
+import com.rails.purchaseplatform.framwork.base.XiaoMiStatusBar;
 import com.rails.purchaseplatform.market.R;
 import com.rails.purchaseplatform.market.adapter.pdetail.ZoomImgVp2Adapter;
 import com.rails.purchaseplatform.market.databinding.ActivityImageZoomBinding;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -77,7 +81,11 @@ public class ImageZoomActivity extends BaseErrorActivity<ActivityImageZoomBindin
 
     @Override
     protected void initialize(Bundle bundle) {
-        setDarkStatusBar(android.R.color.black);
+//        if (XiaoMiStatusBar.isXiaomi())
+//            MIUISetStatusBarLightMode(getWindow(), true);
+//        else
+//            setDarkStatusBar(android.R.color.white);
+
         if (!TextUtils.isEmpty(mImageUrl)) {
             binding.ssivZoomImage.setVisibility(View.VISIBLE);
         } else {
@@ -97,7 +105,9 @@ public class ImageZoomActivity extends BaseErrorActivity<ActivityImageZoomBindin
 
     @Override
     protected int getColor() {
-        return android.R.color.white;
+        return XiaoMiStatusBar.isXiaomi()
+                ? android.R.color.transparent
+                : android.R.color.white;
     }
 
     @Override
@@ -167,6 +177,7 @@ public class ImageZoomActivity extends BaseErrorActivity<ActivityImageZoomBindin
         mConvertToBitmap.setDaemon(false);
         mConvertToBitmap.start();
     }
+
     private void setDarkStatusBar(int color) {
         int statusBarColor;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -178,4 +189,36 @@ public class ImageZoomActivity extends BaseErrorActivity<ActivityImageZoomBindin
         }
         StatusBarCompat.compat(this, statusBarColor);
     }
+
+
+    /**
+     * 设置状态栏字体图标为深色，需要MIUIV6以上
+     *
+     * @param window 需要设置的窗口
+     * @param dark   是否把状态栏字体及图标颜色设置为深色
+     * @return boolean 成功执行返回true
+     */
+    public static boolean MIUISetStatusBarLightMode(Window window, boolean dark) {
+        boolean result = false;
+        if (window != null) {
+            Class clazz = window.getClass();
+            try {
+                int darkModeFlag = 0;
+                Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+                Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+                darkModeFlag = field.getInt(layoutParams);
+                Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+                if (dark) {
+                    extraFlagField.invoke(window, darkModeFlag, darkModeFlag);//状态栏透明且黑色字体
+                } else {
+                    extraFlagField.invoke(window, 0, darkModeFlag);//清除黑色字体
+                }
+                result = true;
+            } catch (Exception e) {
+
+            }
+        }
+        return result;
+    }
+
 }
